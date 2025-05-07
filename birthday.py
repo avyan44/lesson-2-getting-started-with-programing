b5d2567dc15ba8bece39635aeb03aa794575eae5
avyan44 <sumianubha@gmail.com>
2025-05-07 19:26:55 +0100

    birthday.py

---
+++ birthday.py
@@ -0,0 +1,8 @@
+avyan="January 17"
+print("my birthday is on", avyan)
+arnav="october 29"
+print("my brothers birthday is on",arnav)
+anant="august 29"
+print("my dads birthday is on",anant)
+anubha="may 2"
+print("my moms birthday is on",anubha)
