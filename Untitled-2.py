b6c347ac2e8d9e6c3a982c703644080704368251
avyan44 <sumianubha@gmail.com>
2025-05-07 18:19:15 +0100

    birthday

--- Untitled-2.py
+++ Untitled-2.py
@@ -1,4 +1,4 @@
-print("mum birthday=2 may")
-print("dad birthday=29 august")
-print("brother birthday=29 october")
-print("avyan birthday=17 january")
+("mum birthday=2 may")
+("dad birthday=29 august")
+("brother birthday=29 october")
+("avyan birthday=17 january")
